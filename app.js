e21c96719b38fc0355d00af9ba3ef25a67e40bc9
isa <sumerisa5@gmail.com>
2021-08-02 12:19:49 +0300

    day added

--- app.js
+++ app.js
@@ -1,47 +1,48 @@
 function askName() {
-    var name = prompt("Adınız nedir?");
-    document.querySelector("#myName").innerText = name;
+    let name = prompt("Adınız nedir?");
+    document.getElementById("myName").innerText = name;
+ }
+ 
+ function showTime() {
+   let date = new Date();
+   let h = date.getHours();
+   let m = date.getMinutes();
+   let s = date.getSeconds();
+   let day = date.getDay();
+ 
+   switch (day) {
+    case 1:
+        day="Pazartesi";
+        break;
+    case 2:
+        day="Salı";
+        break;
+    case 3:
+        day="Çarşamba";
+            break;
+    case 4:
+        day="Perşembe";
+        break;
+    case 5:
+        day="Cuma"
+        break;
+    case 6:
+        day="Pazartesi";
+        break;
+    case 7:
+        day="Salı";
 }
-
-function showTime() {
-    let date = new date ()
-    let h = date.getHours();
-    let m = date.getMinutes();
-    let s = date.getSeconds();
-    let day = date.getDay();
-
-    switch (day) {
-        case 1:
-            day="Pazartesi";
-            break;
-        case 2:
-            day="Salı";
-            break;
-        case 3:
-            day="Çarşamba";
-                break;
-        case 4:
-            day="Perşembe";
-            break;
-        case 5:
-            day="Cuma"
-            break;
-        case 6:
-            day="Pazartesi";
-            break;
-        case 7:
-            day="Salı";
-    }
-
-    h = h < 10 ? "0" + h : h;
-    m = m < 10 ? "0" + m : m;
-    s = s < 10 ? "0" + s : s;
-
-    let time = h + ":" + m + ":" + s + " " + day;
-    document.querySelector("#myClock").innerText  = time;
-
-    setTimeout (showTime);
-}
-
-askName();
-showTime();
+ 
+   h = h < 10 ? "0" + h : h;
+   m = m < 10 ? "0" + m : m;
+   s = s < 10 ? "0" + s : s;
+ 
+   let time = h + ":" + m + ":" + s + " " + day;
+   document.querySelector("#myClock").innerText = time;
+   document.querySelector("#myClock").textContent = time;
+ 
+   setTimeout(showTime, 1000);
+ }
+ 
+ askName();
+ showTime();
